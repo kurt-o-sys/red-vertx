27af59e2908833ff98b51fa8ff698c0a4fe8a414
kurtsys <kurt.sys@gmail.com>
2014-03-16 08:45:07 +0100

    prepare for Java8

--- src/main/java/red/vertx/server/webservices/WebServiceController.java
+++ src/main/java/red/vertx/server/webservices/WebServiceController.java
@@ -11,27 +11,22 @@ public class WebServiceController
 
    @Override
    public void start() {
-      HttpServer server = vertx.createHttpServer();
-      RouteMatcher routeMatcher = new RouteMatcher();
+      final HttpServer server = vertx.createHttpServer();
+      final RouteMatcher routeMatcher = new RouteMatcher();
 
       final JsonArray routes = container.config().getArray("routes");
 
       for (Object route : routes) {
          final JsonObject r = (JsonObject) route;
-         routeMatcher.get(r.getString("url"), new Handler<HttpServerRequest>() {
-            public void handle(final HttpServerRequest request) {
-               vertx.eventBus().send(
-                     r.getString("channel"),
-                     "message from controller",
-                     new Handler<Message<String>>() {
-                        @Override
-                        public void handle(Message<String> reply) {
-                           request.response().headers().set("Content-Type", "text/plain");
-                           request.response().end(reply.body() + " from " + r.getString("url"));
-                        }
-                     });
+         routeMatcher.get(r.getString("url"), request -> {
+            vertx.eventBus().send(
+                  r.getString("channel"),
+                  "message from controller",
+                  (Handler<Message<Object>>) reply -> {
+                     request.response().headers().set("Content-Type", "text/plain");
+                     request.response().end(reply.body() + " from " + r.getString("url"));
+                  });
 
-            }
          });
       }
       server.requestHandler(routeMatcher).listen(8080);
